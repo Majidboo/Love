782279e12260c0450edcdb21787794b6fa592700
Majidboo <dalandangapr2@gmail.com>
2024-10-18 15:47:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function Click() {
+  document.querySelector(".love").style.zIndex= "100";
+}
+
+window.addEventListener('click',
+function () {
+  var music = document.getElementById("music");
+  music.play();
+}
+);
